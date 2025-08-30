d3ede7184558574a4c3337402b3e7e13a1990970
uuuuday <krishnauday200301@gmail.com>
2025-08-30 19:16:00 +0530

    add teh initial code of setting up

---
+++ src/model/user.ts
@@ -0,0 +1,80 @@
+import mongoose, {Document, Schema } from "mongoose";
+import { Noto_Sans_Mongolian } from "next/font/google";
+
+
+export interface Message extends Document{
+    content:string, // here s is small at interface
+    createdAt:Date,
+}
+
+
+const MessageSchema:Schema<Message>=new mongoose.Schema({
+    content:{
+        type:String,
+        required:true
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now,
+    }
+})
+
+
+
+export interface User extends Document{
+    name:string,
+    email:string,
+    password:string,
+    createdAt:Date,
+    updatedAt:Date,
+    verifyCode:string,
+    isVerified:boolean,
+    isAcceptingMessage:boolean,
+    messages:Message[],
+}
+
+
+
+const UserSchema:Schema<User>=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now
+    },
+    updatedAt:{
+        type:Date,
+        default:Date.now
+    },
+    verifyCode:{
+        type:String,
+        required:true
+    },
+    isVerified:{
+        type:Boolean,
+        default:false
+    },
+    isAcceptingMessage:{
+        type:Boolean,
+        default:true
+    },
+    messages:[MessageSchema]
+})
+
+
+const UserModel =
+mongoose.models.user as mongoose.Model<User>
+ || mongoose.model<User>("User",UserSchema);
+
+export default UserModel;
