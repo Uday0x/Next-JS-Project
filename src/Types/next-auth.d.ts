5aa62fd30a9cd06ab1dda5cc83b91f1cc388f2db
uuuuday <krishnauday200301@gmail.com>
2025-09-06 17:16:33 +0530

    add teh code callbacks sessions and pages

--- src/Types/next-auth.d.ts
+++ src/Types/next-auth.d.ts
@@ -1,31 +1,28 @@
-import "next-auth";
-import { DefaultSession } from "next-auth";
+import 'next-auth';
 
-declare module "next-auth" { //we are amking cahnges in default next auth modiule thats the reason we are using the keyword module
-    interface session {
-        user:{
-            _id?:string;
-            isVerified?:boolean;
-            isAcceptingMessage?:boolean;
-            username?:string;
-        }& DefaultSession['user']
-    }
+declare module 'next-auth' {
+  interface Session {
+    user: {
+      _id?: string;
+      isVerified?: boolean;
+      isAcceptingMessages?: boolean;
+      username?: string;
+    } & DefaultSession['user'];
+  }
+
+  interface User {
+    _id?: string;
+    isVerified?: boolean;
+    isAcceptingMessages?: boolean;
+    username?: string;
+  }
 }
 
-    interface User{
-        _id?:string,
-        isVerified?:boolean,
-        isAcceptingmessages?:boolean,
-        username?:string,
-    }
-
-
-   declare module "next-auth/jwt"
-{
-    interface JWT{
-        _id?:string,
-        isVerified?:boolean,
-        isAcceptingMessages?:boolean,
-        username?:string
-    }
+declare module 'next-auth/jwt' {
+  interface JWT {
+    _id?: string;
+    isVerified?: boolean;
+    isAcceptingMessages?: boolean;
+    username?: string;
+  }
 }
--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -62,5 +62,36 @@ export const authOptions: NextAuthOptions ={
 })
     ],
 
-    
+
+    callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token._id = user._id?.toString(); // Convert ObjectId to string
+        token.isVerified = user.isVerified;
+        token.isAcceptingMessages = user.isAcceptingMessages;
+        token.username = user.username;
+      }
+      return token;
+    },
+
+
+    async session({session , token}){
+          if (token) {
+        session.user._id = token._id;
+        session.user.isVerified = token.isVerified;
+        session.user.isAcceptingMessages = token.isAcceptingMessages;
+        session.user.username = token.username;
+      }
+      return session
+    }
+},
+    session:{
+         strategy:"jwt",
+    },
+    secret:process.env.NEXTAUTH_SECRET,
+
+    pages:{
+        signIn:"/sign-in"
+    }
+
 }
