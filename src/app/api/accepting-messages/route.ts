d4ed1f9e67475e73340cf187e1ddf8911e2d2a44
uuuuday <krishnauday200301@gmail.com>
2025-09-09 19:35:09 +0530

    push another api of getting data of accepting messages

--- src/app/api/accepting-messages/route.ts
+++ src/app/api/accepting-messages/route.ts
@@ -63,3 +63,50 @@ export async function POST(request: Request) {
         );
     }
 }
+
+
+//this is to know whether user is acceptingMessages or not 
+export async function GET(request: Request) {
+  // Connect to the database
+  await dbconnect() ;
+
+  // Get the user session
+  const session = await getServerSession(authOptions);
+  const user = session?.user;
+
+  // Check if the user is authenticated
+  if (!session || !user) {
+    return Response.json(
+      { success: false, message: 'Not authenticated' },
+      { status: 401 }
+    );
+  }
+
+  try {
+    // Retrieve the user from the database using the ID
+    const foundUser = await UserModel.findById(user._id);
+
+    if (!foundUser) {
+      // User not found
+      return Response.json(
+        { success: false, message: 'User not found' },
+        { status: 404 }
+      );
+    }
+
+    // Return the user's message acceptance status
+    return Response.json(
+      {
+        success: true,
+        isAcceptingMessages: foundUser.isAcceptingMessages,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error('Error retrieving message acceptance status:', error);
+    return Response.json(
+      { success: false, message: 'Error retrieving message acceptance status' },
+      { status: 500 }
+    );
+  }
+}
