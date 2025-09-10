8660d193debf2f9b0e3c099c8ebdf8b2ae920f70
uuuuday <krishnauday200301@gmail.com>
2025-09-10 22:02:25 +0530

    push the latest changes

--- src/app/(auth)/sign-In/page.tsx
+++ src/app/(auth)/sign-In/page.tsx
@@ -1,21 +1,43 @@
 "use client"
 
-import { useSession, signIn, signOut } from "next-auth/react"
 
-export default function Component() {
-  const { data: session } = useSession()
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    )
-  }
-  return (
-    <>
-      Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
-    </>
-  )
-}
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
+import { signIn } from "next-auth/react"
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+import { toast } from "sonner"
+import { signUpSchema } from '@/schemas/signUpSchema';
+import { signInSchema } from '@/schemas/signInschemaSchema';
+import { useDebounceValue } from 'usehooks-ts'
+
+
+
+export default function SignUpForm(){
+      //define states 
+      const [username,setUsername]=useState('');
+      const [usernameMessage,setUsernameMessage] = useState("");
+      const[ischeckingusername,setIscheckingusername] =useState(false);
+      const[issubmitting,setIssubmitting] =useState(false);
+
+      const debouncedUsername = useDebounceValue(username,300) //if wondering where i susername cmg from its from teh state which we would handle later
+      
+
+
+
+      const router = useRouter() //this is useful redirecting pages 
+      
+
+      //setting up of the form 
+      const form = useForm<z.infer<typeof signUpSchema>>({
+          resolver:zodResolver(signUpSchema),
+          //default values
+          defaultValues:{
+            username:"",
+            email:"",
+            password:""
+
+          }
+      })
+} 
